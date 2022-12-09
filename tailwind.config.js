24f7bd6f13744ea208feedabd9121b70826048f1
ccpau <chloe@pau.be>
2022-12-09 10:43:10 +0100

    Added Tailwindcss and responsive navbar

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      white: "#fff",
+      black: "#000",
+      "green-500": "#1C463E",
+      "green-300": "#619B8A",
+      "green-200": "#B2C9AB",
+      "mint-400": "#4FB286",
+      "mint-200": "#50FFB1",
+      "mint-100": "#BDF8E6",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
