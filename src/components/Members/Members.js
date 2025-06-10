faeb8b54b66e581a16282f316a5d6fc5c7dbb7ce
claescc <claes_cc@live.be>
2025-06-10 19:07:29 +0200

    Update members & volunteers, push and build

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -7,15 +7,17 @@ const Members = () => {
   const { t } = useTranslation()
   const query = collection(db, "members")
   const [docs, loading] = useCollectionData(query)
+  const activeMembers = docs?.filter(doc => doc.isActive && !doc.isVolunteer);
+  const activeVolunteers = docs?.filter(doc => doc.isActive && doc.isVolunteer);
 
   return (
     <div className="dre-container">
       <div className="dre-title">
-        {t("contact.ourteam")}
+        {t("contact.orgmembers")}
         {loading && "Loading..."}
       </div>
       <div className="flex flex-row flex-wrap justify-center">
-        {docs?.map(doc => (
+        {activeMembers?.map(doc => (
           <div key={doc.firstname} className="h-28 w-32 m-4 flex flex-col">
             <img
               key={doc.avatar}
@@ -29,7 +31,28 @@ const Members = () => {
           </div>
         ))}
       </div>
+
+      <div className="dre-title">
+        {t("contact.orgvolunteers")}
+      </div>
+      <div className="flex flex-row flex-wrap justify-center">
+        {activeVolunteers?.map(doc => (
+          <div key={doc.firstname} className="h-28 w-32 m-4 flex flex-col">
+            <img
+              key={doc.avatar}
+              className="max-h-24 max-w-24 mx-auto rounded-full "
+              src={doc.avatar}
+              alt={doc.firstname}
+            ></img>
+            <div key={doc.lastname} className="text-xs p-1 mx-auto">
+              {doc.firstname} {doc.lastname}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="dre-aligna">{t("sponsorship.info")}</div>
     </div>
+
   )
 }
 
